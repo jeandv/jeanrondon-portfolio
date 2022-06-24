669a6f40a3777a72f05a2866069d9c76053b3c8d
Jean Rondon <jeanrdev@gmail.com>
2022-06-24 17:58:28 -0400

    Update Work.jsx

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
             <h2 className="head-text">Sección de <span>Proyectos</span></h2>
 
             <div className="app__work-filter">
-                {['Web design', 'Web App', 'Mobile App', 'Angular JS', 'React JS', 'All'].map((item, index) => (
+                {['Web design', 'Web App', 'PHP', 'Mobile App', 'Angular JS', 'React JS', 'All'].map((item, index) => (
                     <div
                         key={index}
                         onClick={() => handleWorkFilter(item)}
