af67d628fd9552e98a6b1423f96014759233022c
Jean Rondón <jeanrdev@gmail.com>
2022-08-15 20:48:44 -0400

    Update SocialMedia.jsx

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
-import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 const SocialMedia = () => (
     <div className="app__social">
@@ -14,47 +13,7 @@ const SocialMedia = () => (
                 <BsGithub />
             </a>
         </div>
-        <div>
-            <a href='https://twitter.com/r4yb4' target='https://twitter.com/r4yb4'>
-                <BsTwitter />
-            </a>
-        </div>
-        <div>
-            <a href='https://www.instagram.com/_.jeanr' target='https://www.instagram.com/jeanr.js'>
-                <BsInstagram />
-            </a>
-        </div>
     </div>
 );
 
-=======
-import React from 'react';
-import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
-
-const SocialMedia = () => (
-    <div className="app__social">
-        <div>
-            <a href='https://www.linkedin.com/in/jeandv/' target='https://www.linkedin.com/in/jeandv/'>
-                <BsLinkedin />
-            </a>
-        </div>
-        <div>
-            <a href=' https://github.com/jeandv' target='https://github.com/jeandv'>
-                <BsGithub />
-            </a>
-        </div>
-        <div>
-            <a href='https://twitter.com/r4yb4' target='https://twitter.com/r4yb4'>
-                <BsTwitter />
-            </a>
-        </div>
-        <div>
-            <a href='https://www.instagram.com/_.jeanr' target='https://www.instagram.com/jeanr.js'>
-                <BsInstagram />
-            </a>
-        </div>
-    </div>
-);
-
->>>>>>> 4d0818b (cambios-2)
 export default SocialMedia;
